631d39b2b723b7feded0eff8e6bae0c9fb032c40
omelet-dev <tr.pultz@gmail.com>
2018-07-30 19:05:21 -0400

    Styling components in React.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Person.css';
 
 /* 'props' could be renamed but in the React community is often referred to as props. Props contain passed variables or attributes from JSX. */
 const person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p>I am {props.name} and I am {props.age} years old!</p>
       <p onClick={props.clickHandler}>{props.children}. children refers to any elements in between tag. This could be HTML, other React Components, text, etc.</p>
       <input type="text" onChange={props.inputChangeMethod} value={props.name} />
