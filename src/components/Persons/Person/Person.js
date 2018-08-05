1e87b5d691d76f6e2f0919cf5c53acbbcb35839b
omelet-dev <tr.pultz@gmail.com>
2018-08-05 18:00:49 -0400

    Converted Person and Persons stateless components (or functions) into stateful components (or classes).

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styleClass from './Person.css';
 
-const person = (props) => {
+class Person extends Component {
+  render(){
+    return (
+      <div className={styleClass.Person}>
+        <p>I am {this.props.name} and I am {this.props.age} years old!</p>
+        <p onClick={this.props.clickHandler}>{this.props.children}. children refers to any elements in between tag. This could be HTML, other React Components, text, etc.</p>
+        <input type="text" onChange={this.props.inputChangeMethod} value={this.props.name} />
+      </div>
+    );
+  }
+}
 
-  return (
-    <div className={styleClass.Person}>
-      <p>I am {props.name} and I am {props.age} years old!</p>
-      <p onClick={props.clickHandler}>{props.children}. children refers to any elements in between tag. This could be HTML, other React Components, text, etc.</p>
-      <input type="text" onChange={props.inputChangeMethod} value={props.name} />
-    </div>
-  );
-};
-
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = ( props ) => {
-  return props.people.map((person, index) => {
-    return <Person
-          clickHandler={() => props.deletePerson(index)}
-          name={person.name}
-          age={person.age}
-          key={person.id}
-          inputChangeMethod={(event) => props.nameChange(event, person.id)} />
-      });
+/* Converted stateless component into a stateful one. */
+class Persons extends Component {
+  render(){
+    return this.props.people.map((person, index) => {
+      return <Person
+            clickHandler={() => this.props.deletePerson(index)}
+            name={person.name}
+            age={person.age}
+            key={person.id}
+            inputChangeMethod={(event) => this.props.nameChange(event, person.id)} />
+        });
+    }
 }
 
-export default persons;
+export default Persons;
