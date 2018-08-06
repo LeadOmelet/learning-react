912ebe0f6ec3bb2297298ab7deb41e36c6e1f417
omelet-dev <tr.pultz@gmail.com>
2018-08-05 22:44:32 -0400

    Gave example of Adjacent Elements return.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,13 +3,21 @@ import styleClass from './Person.css';
 
 class Person extends Component {
   render(){
+    /*
     return (
       <div className={styleClass.Person}>
         <p>I am {this.props.name} and I am {this.props.age} years old!</p>
         <p onClick={this.props.clickHandler}>{this.props.children}. children refers to any elements in between tag. This could be HTML, other React Components, text, etc.</p>
         <input type="text" onChange={this.props.inputChangeMethod} value={this.props.name} />
       </div>
-    );
+    );*/
+
+    /* React 16+ Adjacent Elements */
+    return [
+      <p key="1">I am {this.props.name} and I am {this.props.age} years old!</p>,
+      <p key="2" onClick={this.props.clickHandler}>{this.props.children}. children refers to any elements in between tag. This could be HTML, other React Components, text, etc.</p>,
+      <input key="3" type="text" onChange={this.props.inputChangeMethod} value={this.props.name} />
+    ]
   }
 }
 
